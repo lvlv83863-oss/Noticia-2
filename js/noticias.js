13441f8f9276d24a25944c4ab1f5e199a0c006d1
Leticia Vieira Rivelli Miranda <1063235@senacminas.edu.br>
2026-05-14 16:41:40 -0300

    implementar funcionalidade lightbox

--- js/noticias.js
+++ js/noticias.js
@@ -1,6 +1,6 @@
 function alterar_tema(){
     var body = document.body;
-    
+
     if(body.classList.contains("escuro")){
         body.classList.remove("escuro");
         body.classList.add("claro");
@@ -9,4 +9,16 @@ function alterar_tema(){
         body.classList.remove("claro");
         body.classList.add("escuro");
     }
+}
+
+function abrir_lightbox(element){
+   var lightbox = document.getElementById("lightbox");
+   var lightbox_img = document.getElementById("lightbox-img");
+
+   lightbox_img.src = element.src;
+   lightbox.style.display = "flex";
+}
+
+function fechar_lightbox(){
+    document.getElementById("lightbox").style.display = "none";
 }
